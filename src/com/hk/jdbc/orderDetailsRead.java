d5edc054ae1ff853fc84e997d2c9cb7652307142
unknown <saurabh.nagpal@healthkart.com>
2014-07-10 17:08:37 +0530

    test3

--- src/com/hk/jdbc/orderDetailsRead.java
+++ src/com/hk/jdbc/orderDetailsRead.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class orderDetailsRead {
     public static List<String>  orderdDetail() {
-         JdbcConnectionFile.readJdbcprop("select b.gateway_order_id,c.store_variant_name from base_order b join cart_line_item c on b.id=c.base_order_id where gateway_order_id='" + dataArray.get(5) + "'", new ResultSetExtractor<Object>() {
+         JdbcConnectionFile.readJdbcprop("select b.gateway_order_id,c.store_variant_name from base_order b join cart_line_item c on b.id=c.base_order_id where gateway_order_id='" , new ResultSetExtractor<Object>() {
             String email = null;
             String email2 = null;
             List<String> resultquery = new ArrayList<String>();
