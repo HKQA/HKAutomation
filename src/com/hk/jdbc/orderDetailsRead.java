486eef433c6b9130f5162fd1c4e5401ec302a25b
unknown <saurabh.nagpal@healthkart.com>
2014-07-10 16:29:58 +0530

    orderDetails2

---
+++ src/com/hk/jdbc/orderDetailsRead.java
@@ -0,0 +1,38 @@
+package com.hk.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Saurabh
+ * Date: 7/10/14
+ * Time: 4:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class orderDetailsRead {
+    /*public String orderdDetail(){
+    List<String> resultQuery = (List<String>) JdbcConnectionFile.readJdbcprop("select b.gateway_order_id,c.store_variant_name from base_order b join cart_line_item c on b.id=c.base_order_id where gateway_order_id='" + dataArray.get(5) + "'", new ResultSetExtractor<Object>() {
+        String email = null;
+        String email2 = null;
+        List<String> resultquery= new ArrayList<String>();
+
+
+        @Override
+        public Object extract(ResultSet rs) throws SQLException {
+            while (rs.next()) {
+                email = rs.getString("gateway_order_id");
+                email2 = rs.getString("store_variant_name");
+            }
+            resultquery.add(email);
+            resultquery.add(email2);
+            return resultquery;
+        }
+    });
+    for(String string:resultQuery){
+        System.out.println(string);
+    }
+}*/
+}
--- src/com/hk/orderPlacement/ExistingCodPlacement.java
+++ src/com/hk/orderPlacement/ExistingCodPlacement.java
@@ -79,7 +79,7 @@ public class ExistingCodPlacement extends SharedProperties {
         Thread.sleep(3000);
 
 
-        List<String> resultQuery = (List<String>) JdbcConnectionFile.readJdbcprop("select b.gateway_order_id,c.store_variant_name from base_order b join cart_line_item c on b.id=c.base_order_id where gateway_order_id='" + dataArray.get(5) + "'", new ResultSetExtractor<Object>() {
+        /*List<String> resultQuery = (List<String>) JdbcConnectionFile.readJdbcprop("select b.gateway_order_id,c.store_variant_name from base_order b join cart_line_item c on b.id=c.base_order_id where gateway_order_id='" + dataArray.get(5) + "'", new ResultSetExtractor<Object>() {
             String email = null;
             String email2 = null;
             List<String> resultquery= new ArrayList<String>();
@@ -98,7 +98,7 @@ public class ExistingCodPlacement extends SharedProperties {
         });
         for(String string:resultQuery){
          System.out.println(string);
-        }
+        }*/
         for (int i = 4; i < dataArray.size(); i++) {
 
             SharedProperties.driver.navigate().to(PropertyHelper.readProperty("url") + dataArray.get(i));
