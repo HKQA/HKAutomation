4e9cf8bb6a6f8f208d41535564bba5bdac219c37
nitin kukna <nitin.kukna@healthkart.com>
2014-07-22 15:31:40 +0530

    order detail

--- src/com/hk/orderPlacement/OrderDetailsUtil.java
+++ src/com/hk/orderPlacement/OrderDetailsUtil.java
@@ -3,6 +3,9 @@ package com.hk.orderPlacement;
 import com.hk.commonProperties.SharedProperties;
 import com.hk.elementLocators.PaymentPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,9 +16,23 @@ import org.openqa.selenium.By;
 public class OrderDetailsUtil {
 
     private static String xpathGatewayOrderId = "/html/body/div[1]/div[2]/div/div[4]/div[1]/p[2]";
+    private static String orderAmount = "/html/body/div[1]/div[2]/div/div[4]/div[1]/p[3]";
+    private static String totalItem = "/html/body/div[1]/div[2]/div/div[4]/div[1]/p[4]";
+    private static String userName = "/html/body/div[1]/div[2]/div/div[4]/div[3]/p[1]";
 
-    public static String gatewayOrderId() {
+    public static String GatewayOrderId() {
         return SharedProperties.driver.findElement(By.xpath(xpathGatewayOrderId)).getText();
     }
-
+    public static String OrderAmount() {
+        return SharedProperties.driver.findElement(By.xpath(orderAmount)).getText();
+    }
+    public static String TotalItem() {
+        return SharedProperties.driver.findElement(By.xpath(totalItem)).getText();
+    }
+    public static List<WebElement> Item(){
+        return SharedProperties.driver.findElements(By.className("col-sm-5 col-xs-10 item-name"));
+    }
+    public static String UserName(){
+        return SharedProperties.driver.findElement(By.xpath(userName)).getText();
+    }
 }
